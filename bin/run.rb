0f9358accf43883807ecbe52d5e3d6a05ecf4356
Philippe Le Rohellec <philippe@lerohellec.com>
2022-05-21 14:32:30 -0700

    make cache and log dirs configurable

--- bin/run.rb
+++ bin/run.rb
@@ -5,17 +5,25 @@ require 'dotenv/load'
 require 'rss_notifier'
 
 options = {}
-OptionParser.new do |parser|
+parser = OptionParser.new do |parser|
   parser.banner = "Usage: run.rb [options]"
-  parser.on("-c", "--config [PATH]", String,  "Path to config file") { |cfg|  options[:cfg] = cfg  }
-end.parse!
+  parser.on("-h", "--help", "Prints this help") { puts parser; exit 1 }
+  parser.on("-c", "--config PATH", String,  "Path to config file") { |cfg|  options[:cfg] = cfg  }
+end
+parser.parse!
+
+unless options[:cfg]
+  puts parser
+  exit 1
+end
 
 config = YAML.load(File.read(options[:cfg]))
-
+log_dir = config['log_dir']
+cache_dir = config['cache_dir']
 testing = config['testing']
 puts "#{testing ? "TESTING" : "PRODUCTION"} mode is ON"
 
-logger = Logger.new('rssn.log')
+logger = Logger.new("#{log_dir}/rssn.log")
 logger.level = :debug
 logger.formatter = proc do |severity, datetime, progname, msg|
   "#{datetime.strftime('%H:%M:%S')} #{severity[0]}: #{msg}\n"
@@ -23,8 +31,8 @@ end
 RssNotifier::Manager.init_logger(logger)
 
 pusher = RssNotifier::Pusher.new(testing)
-store = RssNotifier::Store.new(testing ? 'test_store.dump' : 'store.dump')
-hit_counter = RssNotifier::HitCounter.new(testing ? 'test_hit_counters.dump' : 'hit_counters.dump')
+store = RssNotifier::Store.new(testing ? "#{cache_dir}/test_store.dump" : "#{cache_dir}/store.dump")
+hit_counter = RssNotifier::HitCounter.new(testing ? "#{cache_dir}/test_hit_counters.dump" : "#{cache_dir}/hit_counters.dump")
 
 Signal.trap("INT") {
   store.age
